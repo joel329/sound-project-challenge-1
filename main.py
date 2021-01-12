46c3862be7df73ddaa1abab1dab14bfb39975ca9
joel329 <72773776+joel329@users.noreply.github.com>
2021-01-12 10:49:04 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+while True:
+    if input.sound_level() >= 1:
+        light.set_all(color.rgb(255,0,0))
---
+++ main.ts
@@ -0,0 +1,6 @@
+while (true) {
+    if (input.soundLevel() >= 1) {
+        light.setAll(color.rgb(255, 0, 0))
+    }
+    
+}
